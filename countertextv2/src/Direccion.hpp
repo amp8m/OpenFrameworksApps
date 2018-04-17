7e14f9101c437d093546deeedfd62440c9807315
amp8m <agnes.pyrchla@gmail.com>
2018-04-17 16:58:27 -0400

    changed counter to text

---
+++ countertextv2/src/Direccion.hpp
@@ -0,0 +1,23 @@
+//
+//  Direccion.hpp
+//  Mueriel Cooper _b
+//
+//  Created by gonza m on 3/17/18.
+//
+
+#ifndef Direccion_hpp
+#define Direccion_hpp
+
+#include <ofMain.h>
+
+class Direccion{
+public:
+    int rotationX;
+    int rotationY;
+    ofColor color;
+    
+    
+    Direccion();
+};
+
+#endif /* Direccion_hpp */
